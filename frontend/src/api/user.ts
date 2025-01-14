190faaf492746704e4ae5faec4e095d309607134
Yan2708 <74718439+Yan2708@users.noreply.github.com>
2025-01-15 00:20:57 +0100

    login

---
+++ frontend/src/api/user.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { API_BASE_URL } from "./constants";
+
+
+export const connect = async (username: string, password: string, csrf:string): Promise<boolean> => {
+    try {
+        const formData = new URLSearchParams();
+        formData.append('username', username);
+        formData.append('password', password);
+        await axios.post(`${API_BASE_URL}/login/`,formData,{
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-CSRFToken': csrf,  // Add CSRF token to headers
+            },
+            withCredentials: true,
+        });
+        return true;
+    } catch (error) {
+      console.error('Error connecting', error);
+      return false;
+    }
+};
+
+export const deconnect = async (csrf: string): Promise<boolean> => {
+    try {
+      await axios.post(`${API_BASE_URL}/logout/`,{
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-CSRFToken': csrf,  // Add CSRF token to headers
+        },
+        withCredentials: true,
+      });
+      return true;
+    } catch (error) {
+      console.error('Error deconnecting:', error);
+      return false;
+    }
+};
+
+export const getCsrfToken = async () => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/csrf-token/`);
+        document.cookie = `csrftoken=${response.data.csrfToken}; path=/; secure; samesite=Lax`;
+        return response.data.csrfToken;
+    } catch (error) {
+        console.error('Error getting csrf:', error);
+        return false;
+    }
+}
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
 	import '../app.css';
   import Navbar from '$lib/navbar.svelte';
+	import Login from '$lib/login.svelte';
+	import { connect, deconnect, getCsrfToken } from '../api/user';
+
+  let connected: boolean = $state(false)
+  let csrf = $state('')
+
+  onMount(async ()=>{
+    csrf = await getCsrfToken()
+  });
+
+  const handleLogin = async (username:string ,password: string) => {
+    connected = await connect(username, password, csrf)
+  }
+
+  const handleLogout = async() => {
+    connected = await deconnect(csrf)
+  }
 </script>
 
-<div class="min-h-screen flex flex-col">
-  <Navbar />
-  <main class="flex-grow">
-    <slot />
-  </main>
-</div>
+
+{#if connected}
+  <div class="min-h-screen flex flex-col">
+    <Navbar handleLogout={handleLogout}/>
+    <main class="flex-grow">
+      <slot />
+    </main>
+  </div>
+{:else}
+  <div class="min-h-screen flex flex-row" style="justify-content: center;align-items: center;height: 100vh;">
+    <Login handleLogin={handleLogin}/>
+  </div>
+{/if}
+
 
